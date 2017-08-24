0898063c31859e109c2cc9ed125893c843b5d67c
Woody Yang <woody.yang@tetrapak.com>
2017-08-24 10:34:48 +0200

    add missing files

---
+++ QnAmazing.xamarin/QnAmazing/QuestionDetailPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace QnAmazing
+{
+    public partial class QuestionDetailPage : ContentPage
+    {
+        public QuestionDetailPage():this(null)
+        {}
+
+        public QuestionDetailPage(QnAMakerResult qna)
+        {
+            InitializeComponent();
+            BindingContext = new QuestionDetailViewModel(qna);
+        }
+    }
+}
---
+++ QnAmazing.xamarin/QnAmazing/QuestionDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace QnAmazing
+{
+    public class QuestionDetailViewModel
+    {
+        public QnAMakerResult Qna { get; set; }
+
+        public QuestionDetailViewModel(QnAMakerResult qna)
+        {
+            Qna = qna;
+        }
+        public double ProgressScore {
+            get {
+                return Qna.Score / 100;
+            }
+        }
+    }
+}
